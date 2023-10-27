f4b70e1b9ab5159f1a748962933be96123fa6c59
Reza Saneiyan <saneiyan.rzw@gmail.com>
2023-10-27 22:52:33 +0330

    exercise implemented

---
+++ Exercise/EX4Compiler.cs
@@ -0,0 +1,42 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+
+namespace Exercise
+{
+    public static class EX4Compiler
+    {
+        public static void CalculateOptimizedMatrix()
+        {
+            Random random = new();
+
+            Matrix<double> cube = Matrix<double>.Build.Dense(1000, 1000, (i, j) => random.Next(1, 1000)).Power(3);
+        }
+
+        public static void CalculateUnoptimizedMatrix()
+        {
+            int n = 1000;
+            int[,] matrix = new int[n, n];
+            int[,] cube = new int[n, n];
+
+            Random random = new();
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = random.Next(1, 1000);
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    for (int k = 0; k < n; k++)
+                    {
+                        cube[i, j] += matrix[i, k] * matrix[k, j];
+                    }
+                }
+            }
+        }
+    }
+}
--- RzwPlayGround/Program.cs
+++ RzwPlayGround/Program.cs
@@ -6,12 +6,42 @@
 #endregion
 
 #region ReachestCustomer Done!
+
 //int[,] cash = { { 6, 7, 8 }, { 3, 4, 5 }, { 1, 2, 3 } };
-//var result = ReachestCustomers.ReachestCustomers.GetReachestCustomer(cash); 
+//var result = ReachestCustomers.ReachestCustomers.GetReachestCustomer(cash);
+
 #endregion
 
 #region RansomeNote Done!
-//var result = RansomeNote.RansomeNote.IsContainNote("abcd", "cac"); 
+
+//var result = RansomeNote.RansomeNote.IsContainNote("abcd", "cac");
+
 #endregion
 
-Console.ReadKey();
+// تعریف ماتریس A با اعداد صحیح
+// محاسبه توان سه ماتریس A
+using Exercise;
+using System.Diagnostics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Stopwatch optimizedStopwatch = new();
+
+        optimizedStopwatch.Start();
+        EX4Compiler.CalculateOptimizedMatrix();
+        optimizedStopwatch.Stop();
+
+        Stopwatch unOptimizedStopwatch = new();
+
+        unOptimizedStopwatch.Start();
+        EX4Compiler.CalculateUnoptimizedMatrix();
+        unOptimizedStopwatch.Stop();
+
+        Console.WriteLine($"optimize elapse time : {optimizedStopwatch.Elapsed} " +
+            $"unoptimize elapse time : {unOptimizedStopwatch.Elapsed}");
+
+        Console.ReadKey();
+    }
+}
