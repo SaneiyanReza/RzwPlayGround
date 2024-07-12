77eb8a5868c96b658ee861949dfa23cf1e867d6b
Saneiyan <saneiyan.rzw@gmail.com>
2024-07-12 16:53:45 +0330

    update to .net 8

--- RansomeNote/RansomeNote.cs
+++ RansomeNote/RansomeNote.cs
@@ -2,21 +2,46 @@
 {
     public class RansomeNote
     {
+        //first solution
+        //public static bool IsContainNote(string str1, string str2)
+        //{
+        //    int numberOfIndex = default;
+
+        //    for (int i = 0; i < str1.Length; i++)
+        //    {
+        //        char indexOfStr1 = str1[i];
+
+        //        numberOfIndex = str2.IndexOf(indexOfStr1);
+
+        //        if (numberOfIndex == -1)
+        //        {
+        //            return false;
+        //        }
+
+        //        str2 = str2.Remove(numberOfIndex, 1);
+        //    }
+
+        //    return true;
+        //}
+
+        //second solution
         public static bool IsContainNote(string str1, string str2)
         {
-            char[] characters = str2.ToCharArray();
-            int numberOfIndex = default;
+            Dictionary<char, int> keyValuePairs = str2
+                .GroupBy(x => x)
+                .ToDictionary(x => x.Key, y => y.Count());
 
-            for (int i = 0; i < str2.Length; i++)
+            for (int i = 0; i < str1.Length; i++)
             {
-                numberOfIndex = str1.IndexOf(characters[i]);
+                 keyValuePairs.TryGetValue(str1[i], out int specificValueOfKey);
 
-                if (numberOfIndex == -1)
+                if (specificValueOfKey > 0)
                 {
-                    return false;
-                }
+                    keyValuePairs[str1[i]]--;
 
-                str1 = str1.Remove(numberOfIndex , 1);
+                    continue;
+                }
+                return false;
             }
 
             return true;
--- RzwPlayGround/Program.cs
+++ RzwPlayGround/Program.cs
@@ -14,55 +14,12 @@
 
 #region RansomeNote Done!
 
-//var result = RansomeNote.RansomeNote.IsContainNote("abcd", "cac");
+var result = RansomeNote.RansomeNote.IsContainNote("bg", "efjbdfbdgfjhhaiigfhbaejahgfbbgbjagbddfgdiaigdadhcfcj");
 
 #endregion
 
-using Exercise;
-using System.Diagnostics;
 
-internal class Program
-{
-    private static void Main(string[] args)
-    {
-        int n = 1000;
-        int[,] matrix = new int[n, n];
+Console.WriteLine(result);
 
-        Random random = new();
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                matrix[i, j] = random.Next(1, n);
-            }
-        }
 
-        Stopwatch optimizedStopwatch = new();
-
-        optimizedStopwatch.Start();
-
-        EX4Compiler.CalculateOptimizedMatrix();
-
-        optimizedStopwatch.Stop();
-
-        Console.WriteLine($"calculate Optimized Matrix: {optimizedStopwatch.Elapsed}");
-
-        optimizedStopwatch.Restart();
-
-        EX4Compiler.CalculateUnoptimizedMatrix(matrix, n);
-
-        optimizedStopwatch.Stop();
-
-        Console.WriteLine($"calculate Unoptimized Matrix: {optimizedStopwatch.Elapsed}");
-
-        optimizedStopwatch.Restart();
-
-        EX4Compiler.CalculateOptimizedMatrixByOpenMp(matrix, n);
-
-        optimizedStopwatch.Stop();
-
-        Console.WriteLine($"calculate Matrix By OpenMp: {optimizedStopwatch.Elapsed}");
-
-        Console.ReadKey();
-    }
-}
+Console.ReadKey();
